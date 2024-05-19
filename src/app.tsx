350250c9d79cd554731bdc5303ecdd7cb9971a3d
Guilherme Farrel de Souza <garcia.farrel@gmail.com>
2024-05-18 23:52:59 -0300

    feat: :mag: add SEO in page

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,28 @@
-import styles from './app.module.css'
-import { Player } from './components/Player'
 import { createRef, useEffect, useState } from 'react'
-import { useMetadata } from './hooks/useMetadata'
-
-import musicFile from './assets/Down South - Jeremy Loops.mp3'
+import DocumentMeta from 'react-document-meta'
 import { DownloadSimple, MusicNote } from '@phosphor-icons/react'
 
+import musicFile from './assets/Down South - Jeremy Loops.mp3'
+
+import styles from './app.module.css'
+import { Player } from './components/Player'
+import { useMetadata } from './hooks/useMetadata'
+
 export const App: React.FC = () => {
+  const meta = {
+    title: '🎵 React Music Player',
+    description:
+      "A simple music player built with React, that can play your favorite songs! Inspired by the #boraCodar's challenges by Rocketseat!",
+    canonical: 'https://music-player.farrel.tech/',
+    meta: {
+      charset: 'utf-8',
+      name: {
+        keywords:
+          'react,music,player,farrel,farreldev,farrel.tech,rocketseat,challenge',
+      },
+    },
+  }
+
   const [source, setSource] = useState<File | null>(null)
   const ref = createRef<HTMLAudioElement>()
 
@@ -28,34 +44,36 @@ export const App: React.FC = () => {
   }
 
   return (
-    <main className={styles.wrapper}>
-      <label className={styles.file}>
-        <MusicNote size={32} weight="regular" />
-        Upload your music
-        <input type="file" accept=".mp3,audio/*" onChange={onFileChange} />
-      </label>
-
-      <a
-        className={styles.download}
-        href={musicFile}
-        download
-        title="Download sample"
-      >
-        <DownloadSimple />
-        <div>
+    <DocumentMeta {...meta}>
+      <main className={styles.wrapper}>
+        <label className={styles.file}>
           <MusicNote size={32} weight="regular" />
+          Upload your music
+          <input type="file" accept=".mp3,audio/*" onChange={onFileChange} />
+        </label>
+
+        <a
+          className={styles.download}
+          href={musicFile}
+          download
+          title="Download sample"
+        >
+          <DownloadSimple />
           <div>
-            <strong>Down South</strong>
-            <span>Jeremy Loops</span>
+            <MusicNote size={32} weight="regular" />
+            <div>
+              <strong>Down South</strong>
+              <span>Jeremy Loops</span>
+            </div>
           </div>
-        </div>
-      </a>
+        </a>
 
-      <Player audioRef={ref} metadata={metadata} />
-      <Player audioRef={ref} metadata={metadata} variant="small" />
-      <Player audioRef={ref} metadata={metadata} variant="small" noTimer />
+        <Player audioRef={ref} metadata={metadata} />
+        <Player audioRef={ref} metadata={metadata} variant="small" />
+        <Player audioRef={ref} metadata={metadata} variant="small" noTimer />
 
-      <audio ref={ref} loop autoPlay />
-    </main>
+        <audio ref={ref} loop autoPlay />
+      </main>
+    </DocumentMeta>
   )
 }
